884296e4aeccfc1ec60b5c694fb0e752e798d189
Shreyash Ramtekkar <1604011shreyash@gmail.com>
2024-10-04 10:56:35 +0100

    Log setup completed

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ logger = fxstreetlogger.get_logger(__name__)
 
 
 def main():
+    print('message')
     logger.debug("This is my message to the world");
 
 
-if __name__ == "main":
+if __name__ == "__main__":
     main()
